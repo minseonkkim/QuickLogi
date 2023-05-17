bda9859b9f47d77c8f7b0cdd3aeb6bee00640c0a
chanx2S2 <cksdud285@gmail.com>
2023-05-17 13:42:49 +0900

    firebase app 등록, gitignore 수정

--- companyapp/lib/main.dart
+++ companyapp/lib/main.dart
@@ -5,9 +5,14 @@ import 'package:flutter_application_1/screens/profile_screen.dart';
 import 'package:flutter_application_1/screens/request_list_screen.dart';
 import 'package:flutter_application_1/screens/splash_screen.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
